40debb9275a096d94296e78db961ba8f9a45e119
Evans <evanschan200@gmail.com>
2020-05-17 00:49:54 +0100

    still on heroku

--- college/settings.py
+++ college/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -146,3 +146,5 @@ LOGIN_REDIRECT_URL = '/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 ACCOUNT_SIGNUP_FORM_CLASS = 'users.forms.SignupForm'
+
+django_heroku.settings(locals())
