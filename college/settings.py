1b008fd6b897b302914a06d09d4158f9acf84287
Evans <evanschan200@gmail.com>
2020-05-17 12:20:32 +0100

    heroku settings altered

--- college/settings.py
+++ college/settings.py
@@ -147,4 +147,24 @@ LOGIN_REDIRECT_URL = '/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 ACCOUNT_SIGNUP_FORM_CLASS = 'users.forms.SignupForm'
 
-django_heroku.settings(locals())
+
+
+#django_heroku.settings(locals())
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+        }
+# Honor the 'X-Forwarded-Proto' header for request.is_secure().
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# Allow all host headers.
+    ALLOWED_HOSTS = ['*']
+# Static asset configuration
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+    STATIC_URL = '/static/'
+    MEDIA_URL = '/media/'
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
