cb01717784380c8ccae719d4171584102303db3b
Evans <evanschan200@gmail.com>
2020-05-17 12:26:29 +0100

    Debug = false for heroku

--- college/settings.py
+++ college/settings.py
@@ -158,7 +158,8 @@ if os.getcwd() == '/app':
 # Honor the 'X-Forwarded-Proto' header for request.is_secure().
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers.
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ['college-sales.herokuapp.com']
+    DEBUG = False
 # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
